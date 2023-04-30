540fcc9ab14acf5fb84b2ead07dd1b7d53a2671a
Рамиля <bzzik@list.ru>
2023-04-30 23:58:51 +0300

    adding services

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repositories.FacultyRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Optional<Faculty> findFaculty(long id) {
+        return facultyRepository.findById(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(long id) {
+        facultyRepository.deleteById(id);
+    }
+
+
+    public Collection<Faculty> findByColor(String color) {
+        return facultyRepository.findAll().stream()
+                .filter(f -> f.getColor().equals(color)).collect(Collectors.toList());
+    }
+
+    public Collection<Faculty> getAll() {
+        return facultyRepository.findAll();
+    }
+}
