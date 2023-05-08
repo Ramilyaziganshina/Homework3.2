9a276c0f00cb1cba950184ff88991c06005e74d2
Рамиля <bzzik@list.ru>
2023-05-08 22:17:36 +0300

    добавлена пагинация для метода getAllAvatars в AvatarController

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("avatar")
@@ -61,4 +62,11 @@ public class AvatarController {
         }
     }
 
+    @GetMapping
+    public ResponseEntity<Collection<Avatar>> getAllAvatars(
+            @RequestParam("page") Integer pageNumber,
+            @RequestParam("size") Integer pageSize) {
+        Collection<Avatar> avatars = avatarService.getAllAvatars(pageNumber, pageSize);
+        return ResponseEntity.ok(avatars);
+    }
 }
