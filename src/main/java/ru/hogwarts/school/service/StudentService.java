36592b1d887b75ffe70cab459cd0d1b17bb7dec0
Рамиля <bzzik@list.ru>
2023-05-11 11:12:05 +0300

    подготовка к заданию

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,64 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public Student editStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> findByAge(int age) {
+        return studentRepository.findAll().stream()
+                .filter(s -> s.getAge() == age).collect(Collectors.toList());
+    }
+
+    public Collection<Student> getAll() {
+        return studentRepository.findAll();
+    }
+
+    public Collection<Student> findByAgeBetween(Integer age, Integer age2) {
+        return studentRepository.findStudentsByAgeBetween(age, age2);
+    }
+
+    public Faculty findFacultyByStudent(long studentId) {
+        return studentRepository.findById(studentId).orElse(null).getFaculty();
+    }
+
+    public double getAverageAge() {
+        return studentRepository.getAverageAge();
+    }
+
+    public int getCountOfStudents() {
+        return studentRepository.getCountOfStudents();
+    }
+
+    public Collection<Student> getFiveLastStudens() {
+        return studentRepository.getFiveLastStudents();
+    }
+}
