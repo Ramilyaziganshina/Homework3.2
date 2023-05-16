3163c406a6b4ddf4224158ac710fe45a9628b9b4
Рамиля <bzzik@list.ru>
2023-05-16 12:31:08 +0300

    Запустились тесты, испрален нулевой порт в дефолтной конфигурации

--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -32,7 +31,7 @@ class StudentControllerTest {
     @MockBean
     private StudentRepository studentRepository;
 
-    @SpyBean
+    @MockBean
     private StudentService studentService;
 
     @Test
